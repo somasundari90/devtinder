727446f6f3c335b6db1dba4ad13af271a71956f6
Soma A N <somasundari90@gmail.com>
2024-11-29 09:57:00 +0530

    S2 EP7 update and delete user APIs

--- src/app.js
+++ src/app.js
@@ -38,6 +38,30 @@ app.get("/user", async (req, res) => {
   }
 });
 
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    await User.findByIdAndDelete(userId);
+    res.send("User Deleted Successfully");
+  } catch (err) {
+    res.status(404).send(`Error while Deleting User: ${err.message}`);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const updatedData = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+    });
+    console.log(updatedData);
+    res.send("User Updated Successfully");
+  } catch (err) {
+    res.status(404).send(`Error while Updating User: ${err.message}`);
+  }
+});
+
 app.get("/feed", async (req, res) => {
   try {
     const users = await User.find({});
